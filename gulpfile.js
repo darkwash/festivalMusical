54f5a3b5d0046d440a3ce97b6fd67b728db4d94c
Hector Vazquez <correo@correo.com>
2024-09-09 14:09:03 -0600

    Incorporando JavaScript en el proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
-import { src, dest, watch } from "gulp";
+import { src, dest, watch, series } from "gulp";
 import gulpSass from "gulp-sass";
 import * as dartSass from "sass";
 
 const sass = gulpSass(dartSass);
-
 export function css(done) {
-  src("src/scss/**/*.scss")
-    .pipe(sass())
-    .pipe(dest("build/css"));
+  src("src/scss/**/*.scss", { sourcemaps: true })
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("build/css", { sourcemaps: "." }));
+  done();
+}
+
+export function js(done) {
+  src("src/js/app.js")
+    .pipe(dest("build/js"));
   done();
 }
 
 export function dev() {
   watch("src/scss/**/*.scss", css);
+  watch("src/js/**/*.js", js);
 }
+
+export default series(js, css, dev);
