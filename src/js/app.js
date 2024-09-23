d515c0f2e8ffd93e92ba46e76d167038ca767445
Hector Vazquez <correo@correo.com>
2024-09-23 12:10:12 -0600

    agregando JS al Header

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
+  navegacionFija();
   crearGaleria();
+  resaltarEnlace();
 });
+function navegacionFija() {
+  const header = document.querySelector("header");
+  const sobreFestival = document.querySelector(".sobre-festival");
+  document.addEventListener("scroll", function () {
+    if (sobreFestival.getBoundingClientRect().bottom < 1) {
+      header.classList.add("fixed");
+    } else {
+      header.classList.remove("fixed");
+    }
+  });
+}
 
 function crearGaleria() {
   const cantidadImagenes = 16;
@@ -10,6 +23,60 @@ function crearGaleria() {
     const imagen = document.createElement("IMG");
     imagen.src = `src/img/gallery/full/${i}.jpg`;
     imagen.alt = "Imagen dj";
+
+    // event handler
+
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
     galeria.appendChild(imagen);
   }
+}
+
+function mostrarImagen(i) {
+  const imagen = document.createElement("IMG");
+  imagen.src = `src/img/gallery/full/${i}.jpg`;
+  imagen.alt = "Imagen dj";
+
+  // genera el modal
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+  modal.onclick = cerrarModal;
+
+  // boton cerrar modal
+  const cerrarModalBtn = document.createElement("button");
+  cerrarModalBtn.textContent = "X";
+  cerrarModalBtn.classList.add("btn-cerrar");
+  cerrarModalBtn.onclick = cerrarModal;
+
+  modal.appendChild(imagen);
+  modal.appendChild(cerrarModalBtn);
+  // Agregar al HTML
+  document.body.appendChild(modal);
+  document.body.classList.add("overflow-hidden");
+}
+
+function cerrarModal() {
+  const modal = document.querySelector(".modal");
+  modal.classList.add("fadeout");
+  setTimeout(() => {
+    document.body.classList.remove("overflow-hidden");
+    modal?.remove();
+  }, 500);
+}
+
+function resaltarEnlace() {
+  document.addEventListener("scroll", function () {
+    const sections = document.querySelectorAll("section");
+
+    // const navLinks = document.querySelectorAll(".navegacion-principal a");
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
+        console.log(section.id);
+      }
+    });
+  });
 }
